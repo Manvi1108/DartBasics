a5be62c7ff9ec80853356e78d64fa0ee06da89b6
Manvi1108 <manvisinghal285@gmail.com>
2022-06-06 14:56:42 +0530

    5

---
+++ ifelsestatement.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+
+void main()
+{
+   int num = stdin.readByteSync();
+   // ignore: deprecated_member_use
+   var myList = List.filled(3, null, growable: false);
+ print(myList);
+}
