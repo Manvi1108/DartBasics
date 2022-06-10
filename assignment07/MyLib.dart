e6c0a7e856b8d27f05c9378e1befcf20dce22661
Manvi1108 <manvisinghal285@gmail.com>
2022-06-10 12:55:26 +0530

    myLib

--- assignment07/MyLib.dart
+++ assignment07/MyLib.dart
@@ -1,1 +1,18 @@
-//
+// Putting functionality (Addition, Subtraction and Multiplication ) in a file
+// Use lambda Expression for Functions.
+
+
+class MyLib
+{
+int? n1;
+int? n2;
+
+MyLib(this.n1,this.n2);
+
+int sum(x, y) => x + y;
+int sub(x, y) => x - y;
+int mul(x, y) => x * y;
+double div(x, y) => x / y;
+int cmp(x,y) => (x<y) ? x : y;
+
+}
