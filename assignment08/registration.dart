8bec9151d822a5d3f1fe739c9773dc08196ed6c4
Manvi1108 <manvisinghal285@gmail.com>
2022-06-11 14:25:15 +0530

    new change

---
+++ assignment08/registration.dart
@@ -0,0 +1,23 @@
+class registration
+{
+  int? id;
+  String? name;
+  void details()
+  {
+    print("ID : ${id}");
+    print("Name : ${name}");
+  }
+
+}
+class student
+{
+
+}
+class training
+{
+  
+}
+void main()
+{
+
+}
